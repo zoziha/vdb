74cc8f965be408969f38cbf290be9589397d3571
zoziha <zuo.zhihua@qq.com>
2021-11-12 12:37:40 +0800

    Add out.json

--- test/vdb_init.f90
+++ test/vdb_init.f90
@@ -4,11 +4,16 @@ program vdb_init
     implicit none
     
     type(json_file) :: json
+    integer :: unit
     
     call json%initialize(verbose=.true., spaces_per_tab=4)
     
     call json%load(filename = "example/launch-file/launch.json")
     
     call json%print()
+    
+    open (newunit=unit, file="example/launch-file/out.json", form="formatted", status="unknown")
+    call json%print(unit)
+    close(unit)
     
 end program vdb_init
