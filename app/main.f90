8548bc93d6f48af283e6790ef89fb67053a3787b
zoziha <zuo.zhihua@qq.com>
2021-11-12 22:48:02 +0800

    Add json.!

--- app/main.f90
+++ app/main.f90
@@ -1,7 +1,7 @@
 !> fpm test vdb_init --runner build\gfortran_2A42023B310FA28D\app\vdb.exe -- ARGS
 program vdb
 
-    use context, only: get_fpm_cmd
+    use context, only: get_fpm_cmd, processing_json
     use stdlib_string_type, only: string_type, char
     implicit none
     type(string_type) :: app
@@ -11,5 +11,7 @@ program vdb
     call get_fpm_cmd(app, args)
     print *, char(app)
     print *, char(args), size(args)
+    call processing_json(app, args)
+    
 
 end program vdb
--- src/context.f90
+++ src/context.f90
@@ -1,37 +1,102 @@
 module context
 
-    use stdlib_string_type, only: string_type, assignment(=)
+    use stdlib_string_type, only: string_type, assignment(=),char
+    use json_module, only: json_file, json_core,json_value
     implicit none
     private
-    
-    public :: get_fpm_cmd
+
+    public :: get_fpm_cmd, processing_json
 
 contains
 
+    !> Parse fpm CMD
     subroutine get_fpm_cmd(app, args)
-    
+
         type(string_type), intent(out) :: app
         type(string_type), intent(out), allocatable :: args(:)
         character(80) :: buffer
         integer :: i, nargs
-        
+
         nargs = command_argument_count()
-        allocate(args(nargs-1))
-        
+        allocate (args(nargs - 1))
+
         call get_command_argument(1, buffer)
         app = trim(buffer)
-        
+
         if (nargs >= 2) then
-        
+
             do i = 2, nargs
-            
+
                 call get_command_argument(i, buffer)
-                args(i-1) = trim(buffer)
-            
+                args(i - 1) = trim(buffer)
+
             end do
+
+        end if
+
+    end subroutine get_fpm_cmd
+
+    !> Parse file dirction
+    subroutine processing_json(app, args)
+
+        type(string_type), intent(in) :: app
+        type(string_type), intent(in) :: args(:)
+        type(string_type) :: dir
+        type(json_file) :: json
+        character(*), parameter :: NL = new_line("")
+        logical :: exist
+        integer :: unit
+
+        dir = "./.vscode/launch.json"
+
+        inquire (file=char(dir), exist=exist)
+
+        call json%initialize(verbose=.true., spaces_per_tab=4)
+
+        if (exist) then
+        
+            call json%load(filename=char(dir))
+            call construct_json_from_fpm(json, app, args)
+            ! call json%print(filename=char(dir))
+            call json%print()
+            
+        else
+        
+            call system("mkdir "//char(dir))
+            call construct_json_from_fpm(json, app, args)
+            call json%print(filename=char(dir))
             
         end if
+
+    end subroutine processing_json
+    
+    subroutine construct_json_from_fpm(json, app, args)
+
+        type(json_file), intent(inout) :: json
+        type(string_type), intent(in) :: app
+        type(string_type), intent(in) :: args(:)
         
-    end subroutine get_fpm_cmd
+        type(json_value), pointer :: configuration
+        type(json_value), pointer :: child
+        character(:), allocatable :: version
+        
+        call json%get("version", version)
+        print *, "version: ", trim(version)
+        
+        associate(type => version)
+        
+        call json%get("configurations", configuration)
+        call json%get_child(configuration, 1, child)
+        
+        if (associated(child)) then
+        
+            call json%get_child(child, 1, child)
+            call json%get(child, version)
+            ! call json%set(child, 'version', char(version//"."//trim(args(1))))
+            
+        end if
+        print *, "type: ", version
+        
+    end subroutine construct_json_from_fpm
 
 end module context
